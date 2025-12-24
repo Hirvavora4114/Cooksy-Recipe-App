ef64bd978bb9a69f898d5d4f8e4d3aae3b7f5568
Hirvavora4114 <hirvavora4114@gmail.com>
2025-12-24 16:02:22 +0530

    Add files via upload

---
+++ global.js
@@ -0,0 +1,20 @@
+/**
+ * @license MIT
+ * @copyright 2023 codewithsadee
+ * @author codewithsadee <mohammadsadee24@gmail.com>
+ */
+
+"use strict";
+
+/**
+ * Add event on multiple elements 
+ * @param {NodeList} $elements NodeList
+ * @param {String} $elements Event type string
+ * @param {Function} callback Callback function
+ */
+
+window.addEventOnElements = ($elements, eventType, callback) => {
+  for (const $element of $elements) {
+    $element.addEventListener(eventType, callback);
+  }
+};
